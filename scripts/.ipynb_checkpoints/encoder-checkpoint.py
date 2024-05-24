619d2c77f65c5ed6183fc57002eb2eaf3c885289
Simon Lee <simon.lee711@gmail.com>
2024-05-24 19:33:41 +0000

    scripts done

---
+++ scripts/.ipynb_checkpoints/encoder-checkpoint.py
@@ -0,0 +1,25 @@
+# Transformers imports
+from transformers import (
+    AutoModel, AutoModelForSequenceClassification, AutoTokenizer, AutoConfig, 
+    DataCollatorWithPadding, TrainingArguments, Trainer, TextClassificationPipeline, 
+    AdamW, get_scheduler
+)
+import torch 
+import numpy as np
+
+def encode_texts(model_name, texts):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+    model.eval()
+    if torch.cuda.is_available():
+        model.to('cuda')
+    embeddings = []
+    with torch.no_grad():
+        for text in texts:
+            encoded_input = tokenizer(text, return_tensors='pt', truncation=True, max_length=512, padding='max_length')
+            if torch.cuda.is_available():
+                encoded_input = {key: val.to('cuda') for key, val in encoded_input.items()}
+            output = model(**encoded_input)
+            cls_embedding = output.last_hidden_state[:, 0, :]
+            embeddings.append(cls_embedding.cpu().numpy())
+    return np.vstack(embeddings)
