aee565e27a9aab2fb46b07b2b11f924727c04acf
Helio <heliohalperin@gmail.com>
2024-06-23 20:23:12 +0000

    bio-lm

--- scripts/.ipynb_checkpoints/encoder-checkpoint.py
+++ scripts/.ipynb_checkpoints/encoder-checkpoint.py
@@ -2,8 +2,9 @@
 from transformers import (
     AutoModel, AutoModelForSequenceClassification, AutoTokenizer, AutoConfig, 
     DataCollatorWithPadding, TrainingArguments, Trainer, TextClassificationPipeline, 
-    AdamW, get_scheduler
+    AdamW, get_scheduler, pipeline, RobertaTokenizerFast
 )
+
 import torch 
 import numpy as np
 
@@ -24,3 +25,23 @@ def encode_texts(model_name, texts):
             embeddings.append(cls_embedding.cpu().numpy())
     print("embeddings are generated")
     return np.vstack(embeddings)
+
+def encode_texts_biolm(model_name, texts):
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base", max_len=512)
+    model = AutoModel.from_pretrained(model_name)
+    model.eval()
+    if torch.cuda.is_available():
+        model.to('cuda')
+    embeddings = []
+    with torch.no_grad():
+        for text in texts:
+            encoded_input = tokenizer(text, return_tensors='pt', truncation=True, max_length=512, padding='max_length')
+            if torch.cuda.is_available():
+                encoded_input = {key: val.to('cuda') for key, val in encoded_input.items()}
+            output = model(**encoded_input)
+            cls_embedding = output.last_hidden_state[:, 0, :]
+            embeddings.append(cls_embedding.cpu().numpy())
+    print("embeddings are generated")
+    return np.vstack(embeddings)
+
+
